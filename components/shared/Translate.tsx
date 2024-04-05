bebb07becbe4da6e210448e0dfdf89e8c0f34553
Joseph <gadiazajoseph18@gmail.com>
2024-04-05 14:00:00 +0800

    just trying something

--- components/shared/Translate.tsx
+++ components/shared/Translate.tsx
@@ -6,27 +6,27 @@ const Translate: React.FC = () => {
   const [selectedLanguage, setSelectedLanguage] = useState("ja");
   const [isLoading, setIsLoading] = useState(true);
 
+  const googleTranslateElementInit = () => {
+    new (window as any).google.translate.TranslateElement(
+      {
+        pageLanguage: "ja", // Change to Japanese language
+        includedLanguages: "en,ja",
+        layout: (window as any).google.translate.TranslateElement.InlineLayout
+          .VERTICAL,
+      },
+      "google_translate_element"
+    );
+
+    // Add event listener for language change
+    (window as any).google.translate.TranslateElement.prototype.onChange =
+      function (e: { target: { value: string } }) {
+        setSelectedLanguage(e.target.value.toString);
+      };
+
+    setIsLoading(false); // Set loading state to false after initialization
+  };
+
   useEffect(() => {
-    const googleTranslateElementInit = () => {
-      new (window as any).google.translate.TranslateElement(
-        {
-          pageLanguage: "ja", // Change to Japanese language
-          includedLanguages: "en,ja",
-          layout: (window as any).google.translate.TranslateElement.InlineLayout
-            .VERTICAL,
-        },
-        "google_translate_element"
-      );
-
-      // Add event listener for language change
-      (window as any).google.translate.TranslateElement.prototype.onChange =
-        function (e: { target: { value: string } }) {
-          setSelectedLanguage(e.target.value.toString);
-        };
-
-      setIsLoading(false); // Set loading state to false after initialization
-    };
-
     const addScript = document.createElement("script");
     addScript.setAttribute(
       "src",
